f5ee8f552883063c05bc427cbc0408814f81170e
kiidii <kiidii@aniname.com>
2024-02-03 11:29:02 +0200

    added cache members

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -17,14 +17,21 @@ export default async function ready (client: Bot, index: number, resolve: Promis
   if (index === 0) {
     const guild = client.guilds.cache.first() as Guild
 
-    const topRatedUserRoles = guild.roles.cache
+    // !Important - We should cache all guild members to use them in the future
+    const guildMembers = await guild.members.fetch()
+
+    const topRatedUserRoles = (await guild.roles.fetch())
       .filter((role) => role.name.startsWith('Top Rated User #'))
       .map((role) => role)
 
     roles.push(...topRatedUserRoles)
+
+    console.log(`${bot.username} #${bot.discriminator} rating manager #${index + 1} (${++readyBotsCounter}) bot is ready! Cached members: ${guildMembers.size}`)
   }
-  
-  console.log(`${bot.username} #${bot.discriminator} rating manager #${index + 1} (${++readyBotsCounter}) bot is ready!`)
+
+  else {
+    console.log(`${bot.username} #${bot.discriminator} rating manager #${index + 1} (${++readyBotsCounter}) bot is ready!`)
+  }
 
   clients.push(client)
 
